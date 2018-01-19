55a5629e8cee47b09c83816eb21e717288848b28
dinesh-solanki <solankid297@gmail.com>
2018-01-19 22:01:23 +0530

     

--- IiS Native/Form1.vb
+++ IiS Native/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class Form1
+﻿Public Class MainFrm
 
+  
+    Private Sub DepositPrintFeeRecieptToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DepositPrintFeeRecieptToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ToolStripStatusLabel1_Click(sender As Object, e As EventArgs) Handles ToolStripStatusLabel1.Click
+
+    End Sub
 End Class
