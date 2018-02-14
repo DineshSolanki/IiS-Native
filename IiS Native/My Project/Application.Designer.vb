9e4b15004416432e0df236ff4f4f2b716e038e67
dinesh-solanki <solankid297@gmail.com>
2018-02-14 10:48:11 +0530

     

--- IiS Native/My Project/Application.Designer.vb
+++ IiS Native/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.IiS_Native.Form1
+            Me.MainForm = Global.IiS_Native.MainFrm
         End Sub
     End Class
 End Namespace
